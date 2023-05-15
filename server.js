62bf9a80db3cfb0e2e92cc75bc2064ccea3f339a
bulkingfire <118399104+bulkingfire@users.noreply.github.com>
2023-05-15 18:15:20 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const db = require("./config/connection");
+
+
+const routes = require("./routes");
+
+
+const app = express();
+const PORT = process.env.PORT || 3150;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+db.once("open", () => {
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
